269902d7ff06add4b4bad34cf223d6a5bc1b4e81
wandermarmf <wandermarmf@gmail.com>
2017-03-15 14:42:08 -0400

    Criação do projeto
    Criação do projeto

---
+++ MuralUFMS-ejb/build/generated-sources/ap-source-output/model/Login_.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.sql.Timestamp;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-15T14:36:20")
+@StaticMetamodel(Login.class)
+public class Login_ { 
+
+    public static volatile SingularAttribute<Login, Timestamp> data;
+    public static volatile SingularAttribute<Login, Boolean> loged;
+    public static volatile SingularAttribute<Login, Long> id;
+
+}
---
+++ dist/gfdeploy/MuralUFMS/MuralUFMS-war_war/Home.jsp
@@ -0,0 +1,22 @@
+<%-- 
+    Document   : Home
+    Created on : 15/03/2017, 10:11:11
+    Author     : wandermar
+--%>
+
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+ <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Mural UFMS xxx</title>
+    </head>
+    
+    <body>
+        <h1>Mural UFMS!</h1>
+        
+        
+    </body>
+
+</html>
